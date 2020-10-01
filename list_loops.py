ab1b163f4cb48cfa9bb4404187a134c9f76c7d9d
Brent_Shierk <bshierk1996@gmail.com>
2020-09-30 20:06:26 -0400

    fixed question 1-4

--- list_loops.py
+++ list_loops.py
@@ -1,3 +1,26 @@
+#Q1
+songs = ["ROCKSTAR", "Do It", "For The Night"]
+
+#Q2
+print(songs[0:1])
+print(songs[-1:])
+
+#Q3
+songs[2] = "The moon"
+# print the list
+print(songs)
+
+#Q4
+
+songs.append('Day Without End')
+songs.append('Pontiac 87')
+songs.append('Worm In Heaven')
+print(songs)
+del songs[0]
+print(songs)
+
+
+
 # 1. Create another list called animals and fill it with 3 animal strings of your choice such as "Cat", "Dog", and "Bird"
 # 2. Add another animal to your list
 # 3. print out the 3rd animal in the list
